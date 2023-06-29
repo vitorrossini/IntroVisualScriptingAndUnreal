ddecf361e558646a13c0235cd6031fd9d947d6f1
Vitor Rossini <vitorrossini@live.com>
2023-06-29 10:43:03 +0200

    Updating

---
+++ Source/UnrealGoals/UnrealGoals.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealGoals.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGoals, "UnrealGoals" );
